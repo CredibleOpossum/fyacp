a2f01eaf42fb875460ac811633c8dc406bbec300
CredibleOpossum <CredibleOpossum@protonmail.com>
2024-09-06 03:18:47 -0400

    add more tests

--- fchess/src/lib.rs
+++ fchess/src/lib.rs
@@ -676,6 +676,7 @@ impl Board {
         move_buffer
     }
     pub fn fen_parser(fen: &str) -> Board {
+        // Doesn't get parse en_passant square.
         let mut board = Board::default();
 
         let mut index: usize = 0;
@@ -861,10 +862,28 @@ mod tests {
     }
 
     #[test]
+    fn perft_strange() {
+        let tables = ChessTables::default();
+        let board =
+            Board::fen_parser("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");
+        let move_count = perft(board, 5, &tables);
+        assert_eq!(move_count, 15_833_292);
+    }
+
+    #[test]
     fn perft_promotion() {
         let tables = ChessTables::default();
         let board = Board::fen_parser("n1n5/PPPk4/8/8/8/8/4Kppp/5N1N b - - 0 1");
         let move_count = perft(board, 5, &tables);
         assert_eq!(move_count, 3_605_103);
+    }
+
+    #[test]
+    fn perft_promotion_pinned() {
+        let tables = ChessTables::default();
+        let board =
+            Board::fen_parser("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8  ");
+        let move_count = perft(board, 4, &tables);
+        assert_eq!(move_count, 2_103_487);
     }
 }
