ebcc6f62e4532ba7918b9932cc647f68224e3993
CredibleOpossum <CredibleOpossum@protonmail.com>
2024-09-14 22:56:22 -0400

    move ai to another thread

--- chess_egui/src/main.rs
+++ chess_egui/src/main.rs
@@ -1,6 +1,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 #![allow(rustdoc::missing_crate_level_docs)] // it's an example
 
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
+
 use colored::Colorize;
 use eframe::egui;
 use egui::{vec2, Color32};
@@ -59,6 +65,36 @@ impl BitBoard {
     }
 }
 
+fn ai_player(board_mutex: Arc<Mutex<Board>>) {
+    let tables = &ChessTables::default();
+    loop {
+        thread::sleep(Duration::from_millis(100)); // mitigate busy waiting
+        let readonly_board;
+        {
+            readonly_board = match board_mutex.try_lock() {
+                Ok(value) => value.clone(),
+                Err(_) => continue,
+            }
+        }
+
+        if readonly_board.turn != COMPUTER_COLOR {
+            continue;
+        }
+        if readonly_board.get_board_state(tables) != BoardState::OnGoing {
+            continue;
+        }
+        let best_move = get_best_move(4, readonly_board, tables); // calculate best move while lock is not obtained,
+
+        let mut board;
+        {
+            board = board_mutex.lock().unwrap();
+        }
+        if board.get_board_state(tables) == BoardState::OnGoing && board.turn == COMPUTER_COLOR {
+            *board = board.move_piece(best_move);
+        }
+    }
+}
+
 static COMPUTER_COLOR: Color = Color::Black;
 fn main() -> eframe::Result {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
@@ -69,17 +105,24 @@ fn main() -> eframe::Result {
     };
 
     let tables = ChessTables::default();
-    let mut board = Board::fen_parser("1k3r2/ppp5/8/8/8/8/PPP5/1K1R4 w - - 1 2");
 
     let mut previous_colormap = 0;
     let mut previous_click: Option<u64> = None;
     let mut color_mask = 0;
 
+    let board_mutex = Arc::new(Mutex::new(Board::fen_parser(
+        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+    )));
+
+    let mut text = board_mutex.lock().unwrap().get_text_representation();
+
+    {
+        let board_mutex_clone = board_mutex.clone();
+        thread::spawn(move || {
+            ai_player(board_mutex_clone);
+        });
+    }
     eframe::run_simple_native("Chess", options, move |ctx, _frame| {
-        if board.get_board_state(&tables) == BoardState::OnGoing && board.turn == COMPUTER_COLOR {
-            let best_move = get_best_move(3, board.clone(), &tables);
-            board = board.move_piece(best_move);
-        }
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.style_mut().spacing.item_spacing = SPACING;
 
@@ -88,12 +131,21 @@ fn main() -> eframe::Result {
                 egui::FontId::new(48.0, eframe::epaint::FontFamily::Proportional),
             );
 
-            let text = board.get_text_representation();
+            let is_lock_open = { board_mutex.try_lock().is_ok() };
+            if !is_lock_open {
+                for position_index in 0..64 {
+                    let _ = egui::Button::new(&text[position_index as usize]);
+                }
+            }
 
+            {
+                text = board_mutex.try_lock().unwrap().get_text_representation();
+            }
             for y in 0..8 {
                 ui.horizontal(|row_ui| {
                     for x in 0..8 {
                         let position_index = 63 - (y * 8 + x);
+
                         let position_bitmask: u64 = 1 << position_index;
 
                         let mut title;
@@ -112,17 +164,23 @@ fn main() -> eframe::Result {
                         }
 
                         if row_ui.add_sized(BUTTON_SIZE, title).clicked() {
-                            color_mask = board
-                                .get_legal_movement_mask_safe(position_index as u8, &tables)
-                                .0;
+                            {
+                                color_mask = board_mutex
+                                    .try_lock()
+                                    .unwrap()
+                                    .get_legal_movement_mask_safe(position_index as u8, &tables)
+                                    .0;
+                            }
                             //color_mask = board.get_pseudolegal_move_mask(position_index as u8);
 
                             if let Some(previous_click_position) = previous_click {
-                                board.try_make_move(
-                                    previous_click_position as u8,
-                                    position_index as u8,
-                                    &tables,
-                                );
+                                {
+                                    board_mutex.try_lock().unwrap().try_make_move(
+                                        previous_click_position as u8,
+                                        position_index as u8,
+                                        &tables,
+                                    );
+                                }
                                 println!(
                                     "{}{}",
                                     human_readable_position(previous_click_position as u8),
@@ -141,15 +199,18 @@ fn main() -> eframe::Result {
                 });
             }
 
-            match board.get_board_state(&tables) {
-                BoardState::Checkmate => {
-                    ui.label(format!("{:?} wins!", board.other_color()));
+            if let Ok(value) = board_mutex.try_lock() {
+                match value.get_board_state(&tables) {
+                    BoardState::Checkmate => {
+                        ui.label(format!("{:?} wins!", value.other_color()));
+                    }
+                    BoardState::Stalemate => {
+                        ui.label("Stalemate!");
+                    }
+                    BoardState::OnGoing => {}
                 }
-                BoardState::Stalemate => {
-                    ui.label("Stalemate!");
-                }
-                BoardState::OnGoing => {}
             }
+
             ui.label("~Zander");
         });
     })
